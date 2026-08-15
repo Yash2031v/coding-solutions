a8d232b2c9c0b6419d664d25b621e9c223e6f81e
Yash Gautam <gyashh2006@gmail.com>
2026-08-15 16:16:16 +0530

    ✅ Delete a Node | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-15

---
+++ hackerrank/medium/print-the-elements-of-a-linked-list-in-reverse/solution.cpp
@@ -0,0 +1,44 @@
+
+
+/*
+ * Complete the 'deleteNode' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* llist, int position) {
+    int count=1;
+    SinglyLinkedListNode* temp = llist;
+    if(llist==NULL){
+        return NULL;
+    }
+    if (position == 0) {
+        SinglyLinkedListNode* temp = llist;
+        llist = llist->next;
+        delete temp;
+        return llist;
+    }
+    while(count!=position){
+        temp=temp->next;
+        count++;
+    }
+    SinglyLinkedListNode* temp1 = temp->next;
+    temp->next=temp->next->next;
+    free(temp1);
+    return llist;
+    
+}
+
