9f9dc9950f171ee177c6817ffc3fbb83a8ea9502
Yash Gautam <gyashh2006@gmail.com>
2026-08-15 16:13:38 +0530

    ✅ Delete a Node | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-15

--- hackerrank/medium/delete-a-node-from-a-linked-list/solution.cpp
+++ hackerrank/medium/delete-a-node-from-a-linked-list/solution.cpp
@@ -1,13 +1,12 @@
 
 
 /*
- * Complete the 'insertNodeAtPosition' function below.
+ * Complete the 'deleteNode' function below.
  *
  * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
  * The function accepts following parameters:
  *  1. INTEGER_SINGLY_LINKED_LIST llist
- *  2. INTEGER data
- *  3. INTEGER position
+ *  2. INTEGER position
  */
 
 /*
@@ -20,19 +19,20 @@
  *
  */
 
-SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* llist, int data, int position) {
-    SinglyLinkedListNode* new_node = new SinglyLinkedListNode(data);
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* llist, int position) {
     int count=1;
     SinglyLinkedListNode* temp = llist;
+    if(llist==NULL){
+        return NULL;
+    }
     while(count!=position){
         temp=temp->next;
         count++;
     }
     SinglyLinkedListNode* temp1 = temp->next;
-    temp->next=new_node;
-    new_node->next=temp1;
+    temp->next=temp->next->next;
+    free(temp1);
     return llist;
-    
     
 }
 
