dde6ba708e6e0def72195b9d333c7e308109ae1e
Yash Gautam <gyashh2006@gmail.com>
2026-09-02 11:54:01 +0530

    ✅ Pow(x, n) | Medium | C++ | Runtime: 0 ms | Memory: 7.9 MB | 2026-09-02

--- leetcode/medium/0050-powx-n/solution.cpp
+++ leetcode/medium/0050-powx-n/solution.cpp
@@ -4,9 +4,12 @@ public:
         if(n==0){
             return 1;
         }
+        if(x==0){
+            return 0;
+        }
         if(n<0){
             
-            return 1/x*myPow(x,n+1);
+            return 1/x * myPow(x,n+1);
         }
         else{
             return x*myPow(x,n-1);
