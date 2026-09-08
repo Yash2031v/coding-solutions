fdf9f317671d851eb6bfb3683dfc25380deb20cd
Yash Gautam <gyashh2006@gmail.com>
2026-09-08 11:46:43 +0530

    ✅ Count Operations to Obtain Zero | Easy | C++ | Runtime: 3 ms (12.10%) | Memory: 9.4 MB | 2026-09-08

---
+++ leetcode/easy/2169-count-operations-to-obtain-zero/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        if(num1==0){
+            return 0;
+        }
+        if(num2==0){
+            return 0;
+        }
+        if(num1>num2){
+            return countOperations(num1-num2,num2)+1;
+        }
+        else{
+            return countOperations(num1,num2-num1)+1;
+        }
+    }
+};
