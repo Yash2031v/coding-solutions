ea5fdd6f40a480275d7817a3d2fcf407d17e3d58
Yash Gautam <gyashh2006@gmail.com>
2026-09-08 10:10:34 +0530

    ✅ Climbing Stairs | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.7 MB | 2026-09-08

--- leetcode/easy/0070-climbing-stairs/solution.cpp
+++ leetcode/easy/0070-climbing-stairs/solution.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-     int helper(int n,vector<int>&dat){
-        if(n==-1){
+     int helper(int n,vector<int>&dat,int cur){
+        if(cur==n+1){
             return 0;
         }
-        if(n==0){
+        if(cur==n){
             return 1;
         }
-        if(dat[n]==0){
-            dat[n]= helper(n-1,dat) + helper(n-2,dat);
+        if(dat[cur]==0){
+            dat[cur]= helper(n,dat,cur+1) + helper(n,dat,cur+2);
         }
-        return dat[n];
+        return dat[cur];
      }
     int climbStairs(int n) {
+        int cur = 0;
         vector<int>dat(n+1);
-        return helper(n,dat);
+        return helper(n,dat,cur);
 
     }
 };
