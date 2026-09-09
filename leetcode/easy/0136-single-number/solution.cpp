59639c7b0497e2577be4178a960929a5a0ba640d
Yash Gautam <gyashh2006@gmail.com>
2026-09-09 21:11:41 +0530

    ✅ Single Number | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 20.6 MB | 2026-09-09

---
+++ leetcode/easy/0136-single-number/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int sing = 0;
+        for(int i=0;i<nums.size();i++){
+            sing = sing^nums[i];
+        }
+        return sing;
+    }
+};
